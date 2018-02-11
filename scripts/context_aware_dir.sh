589c3c287e8e8b1d4752247dcc7791f0cecec354
Daniel Clarke <u8sand@gmail.com>
2018-02-11 11:18:24 -0500

    Added context_aware_dir fallback via pwdx

--- scripts/context_aware_dir.sh
+++ scripts/context_aware_dir.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # Basically we look for a valid path in the title of the current application
 #  to become the working directory for a command call.
 # In order for it to work, the *full* path must naturally appear in the title
@@ -7,10 +8,20 @@
 #  some extension to the regular expression it probably could be made to work
 #  but since I never put spaces in my paths, I don't feel the need to do it.
 
+# Primary: Find path from title
+#  This is the best way, given that the program
+#  in question puts full paths in the title.
+# Fallback: Find path from process
+#  This is a way usually doesn't work because most
+#  programs don't change from their startup directory.
+#  Still, if it isn't HOME, it's likely the startup
+#  directory of the program is also the directory
+#  you're looking for.
+
 export conf=$(cd $(dirname $0)/.. ; pwd -P)
 source "$conf/settings/env.sh"
 
-i3-msg -t get_tree | python3 -c "
+PWD=$(i3-msg -t get_tree | python3 -c "
 import os
 import re
 import sys
@@ -19,17 +30,30 @@ import jsonpath
 
 props = jsonpath.jsonpath(
   json.load(sys.stdin),
-  '$..nodes[?(@.focused)].window_properties',
+  '$..nodes[?(@.focused)]',
 )[0]
 
-m = re.search(r'~?(/[^ \/]+)+', props['title'])
-ms = m.group(0)
+m = re.search(r'~?(/[^ \/]+)+', props['window_properties']['title'])
+ms = m.group(0) if m else None
 if ms:
   ms = ms.replace('~', os.environ['HOME'])
-if os.path.isdir(ms):
-  print(ms)
-elif os.path.isfile(ms):
-  print(os.path.dirname(ms))
-else:
-  print('')
-"
+  if os.path.isdir(ms):
+    print(ms)
+    exit(0)
+  elif os.path.isfile(ms):
+    print(os.path.dirname(ms))
+    exit(0)
+print(props['window'])
+exit(1)
+")
+
+if [ "$?" -eq 1 ]; then
+  PWD=$(pwdx \
+        $( \
+          xprop -id ${PWD} \
+          | awk -F'=' '/^_NET_WM_PID/{print $2}' \
+        ) | awk -F': ' '{print $2}' \
+      )
+fi
+
+echo "${PWD}"
