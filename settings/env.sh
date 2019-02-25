b7d36650cd369140ff1f46243ed90ab3b186ef6b
Daniel Clarke <u8sand@gmail.com>
2019-02-25 09:43:03 -0500

    Fixes, improvements, and additions to config

--- settings/env.sh
+++ settings/env.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
 
-# Setup bash environment so that scripts work
-#  as intended on your machine. (e.g. making
-#  python3 available and such)
-
-eval "$(pyenv init -)"
-pyenv shell 3.6.1
