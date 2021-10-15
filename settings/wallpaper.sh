e77106613f76ebf2c5541546e0ec383595e202b6
Daniel Clarke <u8sand@gmail.com>
2021-10-15 09:30:39 -0400

    Some system specific tweaks

--- settings/wallpaper.sh
+++ settings/wallpaper.sh
@@ -2,5 +2,5 @@
 
 conf=$(cd $(dirname $0)/.. ; pwd -P)
 export dir="$HOME/Pictures/Wallpapers"
-export disps=(DVI-I-1 DVI-I-2)
+export disps=(DP-5 DP-1)
 export save_current="$conf/.current_wallpaper"
